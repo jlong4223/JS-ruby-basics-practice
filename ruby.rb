e068c3b1fac592a3bcfbb971f1f56ac1b881f312
Jared <jlong4223@gmail.com>
2021-02-05 23:04:02 -0600

    does a ruby challenge

--- ruby.rb
+++ ruby.rb
@@ -3,3 +3,34 @@ def hello
 end
 
 puts hello
+
+
+puts "======================"
+
+def howManySeconds num
+    num * 3600
+end
+
+puts howManySeconds(2)
+
+puts "======================"
+
+def giveMeSomething name
+    name + " is the coolest"
+end
+puts giveMeSomething("Jared")
+
+def leap_year year
+    if year % 4 == 0
+      if year % 100 == 0
+        if yearVar % 400 == 0
+          return true
+        end
+        return false
+      end
+      return true
+    end
+    false
+  end
+
+puts leap_year(2020)
