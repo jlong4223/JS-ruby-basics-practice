35f7b68048d4d7fa6c5a97a6c9c55ddc62cd2d21
Jared <jlong4223@gmail.com>
2021-02-19 20:53:06 -0600

    completes ruby and js challenge

--- ruby.rb
+++ ruby.rb
@@ -83,3 +83,18 @@ def multiplyArray
 end
 
 multiplyArray
+
+# ================================
+# Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+def rental_car_cost(d)
+  if d >= 7
+    puts (d * 40) - 50
+  elsif d >= 3 && d < 7
+    puts (d * 40) - 20
+  elsif d < 3 && d < 7
+    puts d * 40
+  end
+end
+
+rental_car_cost(2)
--- vanilla-js.js
+++ vanilla-js.js
@@ -63,3 +63,18 @@ function multiplyArray() {
 }
 
 multiplyArray();
+
+// ========================================
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+function rentalCarCost(d) {
+  if (d >= 7) {
+    return d * 40 - 50;
+  } else if (d >= 3 && d < 7) {
+    return d * 40 - 20;
+  } else if (d < 7 && d < 3) {
+    return d * 40;
+  }
+}
+
+console.log("rental car cost: " + rentalCarCost(2));
