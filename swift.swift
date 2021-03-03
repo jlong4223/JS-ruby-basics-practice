d9c36cb2fe4b9c80b62698d474873ffcd3cfa60b
Jared Long <jlong4223@gmail.com>
2021-03-02 21:13:50 -0600

    uppercases string in swift

---
+++ swift.swift
@@ -0,0 +1,8 @@
+// a function that converts a string to uppercase
+func upperCase(name: String) -> String{
+  let person = name.uppercased()
+  return(person)
+}
+
+print(upperCase(name: "jared"))
+
