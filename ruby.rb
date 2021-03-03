c600aa39304cbd7d98ddf0acf641b3e94076b984
Jared Long <jlong4223@gmail.com>
2021-03-02 20:52:49 -0600

    fights the dragon rb and js

--- ruby.rb
+++ ruby.rb
@@ -182,3 +182,14 @@ def positive_arr_sum arr
   arr.select(&:positive?).sum
 end 
 puts positive_arr_sum([0, 3, 3, -9])
+
+# ==================================
+# A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+# Return True if yes, False otherwise
+
+def hero bullets, dragons
+  bullets >= dragons * 2 ? true : false
+end
+
+puts hero 4,2
