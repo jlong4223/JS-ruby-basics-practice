39dde97f5a575a65bb6379362317afeb093ba37e
Jared Long <jlong4223@gmail.com>
2021-03-20 20:46:19 -0500

    does a swift name function

--- swift.swift
+++ swift.swift
@@ -1,3 +1,10 @@
+// ========================
+func sayName(name: String)-> String{
+  return(name)
+}
+print(sayName(name: "Jared " + "is my name"))
+
+// ========================
 // a function that converts a string to uppercase
 func upperCase(name: String) -> String{
   let person = name.uppercased()
