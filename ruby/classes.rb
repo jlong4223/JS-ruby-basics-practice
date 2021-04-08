451ae8593dd6b11dc5ee2b6f06622580b787c68c
Jared Long <jlong4223@gmail.com>
2021-04-08 17:24:17 -0500

    makes another new class

--- ruby/classes.rb
+++ ruby/classes.rb
@@ -7,10 +7,16 @@ class Muppet
         @color = color
         puts "This muppet is called #{name}"
         puts "The color is #{color}"
-
     end
 
+    def name
+        @name
+    end
 end
 
 # creating a new class and assigning the name, and a random color to it
 new_class = Muppet.new("Todd", ["red", "green", "orange", "blue"].sample)
+puts new_class.name
+
+another_new_class = Muppet.new("George", ['orange', 'silver', 'white', 'brown'].sample)
+puts another_new_class.name
