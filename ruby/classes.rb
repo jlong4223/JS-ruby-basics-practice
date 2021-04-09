df5c7ef10bdd09421af72b909444cde78b9c2b3a
Jared Long <jlong4223@gmail.com>
2021-04-08 20:06:04 -0500

    adds a Birds class and creates a new one

--- ruby/classes.rb
+++ ruby/classes.rb
@@ -20,3 +20,23 @@ puts new_class.name
 
 another_new_class = Muppet.new("George", ['orange', 'silver', 'white', 'brown'].sample)
 puts another_new_class.name
+
+# =========================================
+class Birds
+    SPECIES = ['Belted Kingfisher', 'Yellow-Billed Cuckoo', 'White-Cheeked Pintail', 'Cinnamon Teal', 'Lesser Scaup', 'Bufflehead', 'Common Goldeneye', 'Fulvous Whistling Duck', 'Hooded Merganser', 'White-Winged Scoter', 'Chimney Swift', 'Rufous Hummingbird', 'Chuck-Wills-Widow', 'Whip-Poor-Will', 'Albatross', 'Ruddy Turnstone', 'Piping Plover', 'Killdeer', 'Laughing Gull', 'Marbled Godwit', 'Least Tern', 'New World Warbler']
+
+    def initialize(adjective, name)
+        @adjective = adjective
+        @name = name
+        @type = SPECIES.sample
+
+        puts "#{name} is an #{adjective} bird!"
+    end
+
+    def type
+        print "#{@name} is an #{@type}"
+    end
+end
+
+the_bird = Birds.new("fun", "Wally")
+puts the_bird.type
